27750042556ecceecffe466b890d3ab394f25aca
Replit user <>
2023-08-17 19:00:45 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -80,7 +80,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
   int i=0, j=0, k=0;
 
   while(i<size1 && j<size2){
-    if(arr1[i]>arr2[j]){
+    if(arr1[i]<arr2[j]){
       result[k]=arr1[i];
       i++;
     }
@@ -100,6 +100,7 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
     j++;
     k++;
   }
+  
 }
 
 /*
